9bdfbcbbea898c8c944e0230d6d84d90e53d5d7e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-02 22:16:30 +0900

    array の追加

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,13 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One.",
+        "Three + Eight is less than Ten."
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = "Four + Two is equal to Six."
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
     }
     
 }
