4061e71ca4e64f89965a648acff1b0c9f878642d
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-03 21:45:11 +0900

    2D array の追加

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One.",
-        "Three + Eight is less than Ten."
+        ["Four + Two is equal to Six.","True"],
+        ["Five - Three is greater than One.","True"],
+        ["Three + Eight is less than Ten.","False"]
     ]
     
     var questionNumber = 0
@@ -29,12 +29,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
         updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
