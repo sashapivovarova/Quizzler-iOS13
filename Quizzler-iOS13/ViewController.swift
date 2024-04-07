2ad8cd0a714c3ebe2f76c9da104338edaac31af3
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-04-07 21:50:07 +0900

    Timer の追加

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -43,16 +43,24 @@ class ViewController: UIViewController {
         let userAnswer = sender.currentTitle
         let actualAnswer = quiz[questionNumber].answer
         
+        if userAnswer == actualAnswer {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
         if questionNumber + 1 < quiz.count {
             questionNumber += 1
         } else {
             questionNumber = 0
         }
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
 }
